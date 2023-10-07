c88fbca271f156425e675c719361fa62e79b3792
BoyLrr <65436246+BoyLrr@users.noreply.github.com>
2023-10-07 20:46:15 +0800

    部署调试

--- src/main/java/com/lee/project/MyApplication.java
+++ src/main/java/com/lee/project/MyApplication.java
@@ -4,11 +4,26 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("com.lee.project.mapper")
 @EnableDubbo
-public class MyApplication {
+public class MyApplication implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    //全局配置跨域cookie传值问题
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://user.senlou.top")
+                .allowCredentials(true);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
